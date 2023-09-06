991d14b58fe82cd305138e2909f0d903fa3695fd
Arnab199923 <96194042+Arnab199923@users.noreply.github.com>
2023-09-06 17:12:18 +0530

    error for location

--- script.js
+++ script.js
@@ -1,66 +1,79 @@
-const inputBox =document.querySelector('.inputBox');
-const search = document.getElementById('searchBtn');
-const weatherImage = document.querySelector('.weatherImg');
-const temparature = document.querySelector('.temp');
-const weatherDes = document.querySelector('.description');
-const humid = document.getElementById('humid');
-const wind = document.getElementById('windSpeed');
-const feels = document.getElementById('feel');
-// const backgroundColor = document.querySelector('container');
+    const inputBox =document.querySelector('.inputBox');
+    const search = document.getElementById('searchBtn');
+    const weatherImage = document.querySelector('.weatherImg');
+    const temparature = document.querySelector('.temp');
+    const weatherDes = document.querySelector('.description');
+    const humid = document.getElementById('humid');
+    const wind = document.getElementById('windSpeed');
+    const feels = document.getElementById('feel');
+    const er = document.querySelector('.notFound');
+    const wbody = document.querySelector('.weatherBody')
+    // const backgroundColor = document.querySelector('container');
 
 
 
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-async function checkWeather(city){
+    async function checkWeather(city){
     const Key = "fe3a5df33b4ef9109aee0b3addba071e";  
-    
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Key}`;
-  
-  const data = await fetch(`${url}`).then(response => response.json()); 
-  
-  console.log(data);
 
-temparature.innerHTML = Math.round(data.main.temp - 273.15)+"\u00b0c";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Key}`;
 
-   weatherDes.innerHTML = data.weather[0].main;
+    const data = await fetch(`${url}`).then(response => response.json()); 
+
+    if(data.cod===`404`){
+        er.style.display="flex";
+        wbody.style.display="none";
+        console.log("error");
+    }else{
+        er.style.display="none";
+        wbody.style.display="flex";
+    }
+
+    console.log(data);
+
+   
+    temparature.innerHTML = Math.round(data.main.temp - 273.15)+"\u00b0c";
+    weatherDes.innerHTML = data.weather[0].main;
     wind.innerHTML = data.wind.speed+'\u2190';
     humid.innerHTML = data.main.humidity+'\&#37';
     feels.innerHTML =Math.round(data.main.feels_like-273.15)+"\u00b0c"
 
+
+
     switch (data.weather[0].main) {
-        case 'Clouds':
-            weatherImage.src = "/image/clouds.png";
-            document.querySelector('.container').style.backgroundColor = "#808080";
-            break;
-            case 'Haze':
-                weatherImage.src= "/image/haze.png";
-                document.querySelector('.container').style.backgroundColor = "#D3D3D3";
-                break;
-                case 'Clear':
-                    weatherImage.src= "/image/sunny.png";
-                    document.querySelector('.container').style.backgroundColor = "#5fff";
-                    break;
-                    case 'Rain':
-                        weatherImage.src= "/image/rain.png";
-                        document.querySelector('.container').style.backgroundColor = " #000999";
-                        // document.querySelector('.container').style.oundColor = " #fff";
-                        break;
-                        case 'Snow':
-                            weatherImage.src= "/image/snow.jpg";
-                            document.querySelector('.container').style.backgroundColor = "#fff";
-                            break;
-                            case 'Mist':
-                                weatherImage.src= "/image/mist.png";
-                                document.querySelector('.container').style.backgroundColor = "lime";
+    case 'Clouds':
+    weatherImage.src = "/image/clouds.png";
+    document.querySelector('.container').style.backgroundColor = "#808080";
+    break;
+    case 'Haze':
+    weatherImage.src= "/image/haze.png";
+    document.querySelector('.container').style.backgroundColor = "#D3D3D3";
+    break;
+    case 'Clear':
+    weatherImage.src= "/image/sunny.png";
+    document.querySelector('.container').style.backgroundColor = "#5fff";
+    break;
+    case 'Rain':
+    weatherImage.src= "/image/rain.png";
+    document.querySelector('.container').style.backgroundColor = " #000999";
+    // document.querySelector('.container').style.oundColor = " #fff";
+    break;
+    case 'Snow':
+    weatherImage.src= "/image/snow.jpg";
+    document.querySelector('.container').style.backgroundColor = "#fff";
+    break;
+    case 'Mist':
+    weatherImage.src= "/image/mist.png";
+    document.querySelector('.container').style.backgroundColor = "lime";
 
 
 
     }
-}
+    }
 
 
-search.addEventListener('click',()=>{
+    search.addEventListener('click',()=>{
     checkWeather(inputBox.value)
-});
+    });
 
